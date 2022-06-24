27659b04d11ca58b5a9ea0fe1c26ac07c1e21520
Antek <antekyt123@gmail.com>
2022-06-24 16:29:54 +0200

    user service improvements2

--- projektApbd/Server/Services/IUserService.cs
+++ projektApbd/Server/Services/IUserService.cs
@@ -4,7 +4,7 @@ namespace projektApbd.Server.Services
 {
     public interface IUserService
     {
-        public bool IsUserExists(string username);
+        public Task<bool> IsUserExists(string username);
         public Task<User> GetUser(string username);
         public Task AddUser(Shared.Models.DTOs.User user);
         public Task DeleteUser(Shared.Models.DTOs.User user);
